51d5d3ea303e6f8306c2369437bfe37387dbdf54
lthero <70460095+lthero-big@users.noreply.github.com>
2024-05-30 04:25:56 +0000

    Update ShangHaiAuto.py

--- ShangHaiAuto.py
+++ ShangHaiAuto.py
@@ -4,13 +4,16 @@ import json
 import yaml
 import time
 import random
+from datetime import datetime
+import argparse
+from bs4 import BeautifulSoup
 
 getToken_url = 'https://qcsh.h5yunban.com/youth-learning/cgi-bin/login/we-chat/callback'
 getUserInfo_url = 'https://qcsh.h5yunban.com/youth-learning/cgi-bin/user-api/course/last-info'
 getClass_url = 'https://qcsh.h5yunban.com/youth-learning/cgi-bin/common-api/course/current'
 checkin_url = 'https://qcsh.h5yunban.com/youth-learning/cgi-bin/user-api/course/join'
 getPersonalInfo_url = 'https://qcsh.h5yunban.com/youth-learning/cgi-bin/user-api/info'
-
+signin_url = "https://qcsh.h5yunban.com/youth-learning/cgi-bin/user-api/sign-in/records"
 headers = {
     'Content-Type': 'text/plain'
 }
@@ -77,6 +80,27 @@ def signup(accessToken, checkinData):
         print('错误信息：' + str(result["message"]))
         return result["message"]
 
+def daily_sign_in(accessToken):
+    # 获取当前日期，并格式化为"YYYY-MM"
+    date = datetime.now().strftime("%Y-%m")
+
+    params = {
+        "accessToken": accessToken['accessToken'],
+        "date": date
+    }
+
+    # 发起GET请求
+    checkin = requests.get(signin_url, params=params, headers=headers)
+    result = checkin.json()
+    
+    if result["status"] == 200:
+        print("每日签到成功")
+        return 1
+    else:
+        print('出现错误，错误码：' + str(result["status"]))
+        print('错误信息：' + str(result["message"]))
+        return result["message"]
+
 
 def getPersonalInfo(accessToken):
     # 获得个人信息
@@ -86,26 +110,6 @@ def getPersonalInfo(accessToken):
     # return info['result']['score']
 
 
-# 调用接口发送邮件，不提供接口
-def sendMail(user,info,resStatus):
-    msg=resStatus
-    if resStatus==1:
-        msg="完成"
-    # 接收方
-    receiver = user['user']['mail']
-    # 内容
-    content = '{updateTime} 您好，{user}!本周大学习打卡：{msg}。当前分数为{score}分'.format(user=info['nickname'],msg=msg,score=info['score'],updateTime=info['lastUpdTime'])
-    params = {
-        'reciever': receiver,
-        # 邮件标题
-        'title': f'[{msg}]青年大学习打卡',
-        # 主要内容
-        'content': content,
-        # 内部大标题
-        'innerTitle': f'青年大学习'
-    }
-    requests.post(url='http://邮件接口/', data=params)
-    print("邮件发送完成")
 
 # 本地发送邮件，需要自行配置
 def sendMail(to_email, title, content):
@@ -138,7 +142,54 @@ def sendMail(to_email, title, content):
         error_msg = '邮件发送异常, {}'.format(str(e))
     print(error_msg)
     return False
+
+
+# 调用api接口方法，自行研究
+def sendMail(user, info, resStatus, task_type):
+    task_message = "每日签到" if task_type == "daily" else "本周大学习"
+    msg = "完成" if resStatus == 1 else "未完成"
+    # 接收方
+    receiver = user['user']['mail']
+    # 内容
+    content = '{updateTime} 您好，{user}! {task_message}完成：{msg}。当前分数为{score}分'.format(
+        user=info['nickname'],
+        task_message=task_message,
+        msg=msg,
+        score=info['score'],
+        updateTime=info['lastUpdTime']
+    )
+    print(content)
+    params = {
+        'reciever': receiver,
+        # 邮件标题
+        'title': f'[{msg}] {task_message}打卡',
+        # 主要内容
+        'content': content,
+        # 内部大标题
+        'innerTitle': f'青年大学习',
+    }
+    response = requests.post(url='https://自行接入api', data=params, verify=True)
+    print("邮件发送状态：", response.status_code)
+
+
+def get_screenshot_url():
+    response = requests.get('https://m.cyol.com/gb/channels/vrGlAKDl/index.html')
+    response.raise_for_status()  # 检查请求是否成功
+    soup = BeautifulSoup(response.text, 'html.parser')
+    href_list = [a['href'] for a in soup.select('body section div ul a') if 'href' in a.attrs]
     
+    if not href_list:
+        raise ValueError("No href found in the specified location")
+    
+    first_href = href_list[0]
+    
+    if 'index.html' in first_href:
+        return first_href.replace("index.html", "images/end.jpg")
+    elif 'm.html' in first_href:
+        return first_href.replace("m.html", "images/end.jpg")
+    else:
+        raise ValueError("Unexpected href format")
+
 if __name__ == "__main__":
     config = getYmlConfig()
     for index, eachuser in enumerate(config['users']):
@@ -157,3 +208,56 @@ if __name__ == "__main__":
             # 需要自行配置发送邮箱
 #             sendMail(eachuser['user']['mail'], "邮件标题", "邮件内容")
         print('===========================================')
+
+if __name__ == "__main__":
+    # 解析命令行参数
+    parser = argparse.ArgumentParser(description="Youth Learning Automation")
+    parser.add_argument('--daily', action='store_true', help="执行每日签到并发送邮件")
+    parser.add_argument('--course', action='store_true', help="执行大学习打卡并发送邮件")
+    parser.add_argument('--savePic', action='store_true', help="保存截图")
+    args = parser.parse_args()
+    
+    if args.savePic:
+        screenshot_url = get_screenshot_url()
+        screenshot_response = requests.get(screenshot_url)
+        with open('./output.jpg', 'wb') as f:
+            f.write(screenshot_response.content)
+        print("截图保存成功")
+    
+    if not args.daily and not args.course:
+        print("错误: 至少需要传入 --daily 或 --course 参数")
+        print("使用示例:")
+        print("每日签到    python index.py --daily")
+        print("大学习打卡  python index.py --course")
+        exit(1)
+
+    config = getYmlConfig()
+    for index, eachuser in enumerate(config['users']):
+        print(eachuser['user']['name'], 'openId为 ', eachuser['user']['openid'])
+        openid = {
+            'appid': 'wx56b888a1409a2920',
+            'openid': eachuser['user']['openid']
+        }
+        accesstoken = getToken(openid)
+
+        if accesstoken:
+            if args.daily:
+                # 每日签到
+                resStatus = daily_sign_in(accesstoken)
+                # 需要自行配置发送邮箱
+                #sendMail(eachuser, getPersonalInfo(accesstoken), resStatus, "daily")
+                # 需要自行配置接口
+                #sendMail(eachuser,personalInfo,resStatus)
+
+            if args.course:
+                # 大学习打卡
+                checkindata = getinfo(accesstoken)
+                if checkindata:
+                    resStatus = signup(accesstoken, checkindata)
+                    # 需要自行配置发送邮箱
+                    #sendMail(eachuser, getPersonalInfo(accesstoken), resStatus, "course")
+
+        print('===========================================')
+        
+        t = random.randint(1, 3)
+        time.sleep(t)
